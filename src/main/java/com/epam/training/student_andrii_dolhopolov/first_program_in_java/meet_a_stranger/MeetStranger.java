26acaf027fbec76ca491b31d9759eed3321ef966
Andrii Dolhopolov <dolgopolov_a@ukr.net>
2023-01-25 00:20:29 +0200

    First programs in Java

---
+++ src/main/java/com/epam/training/student_andrii_dolhopolov/first_program_in_java/meet_a_stranger/MeetStranger.java
@@ -0,0 +1,11 @@
+package com.epam.training.student_andrii_dolhopolov.first_program_in_java.meet_a_stranger;
+
+import java.util.Scanner;
+
+public class MeetStranger {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String InputString = scan.nextLine();
+        System.out.println("Hello, " + InputString);
+    }
+}
---
+++ src/main/java/com/epam/training/student_andrii_dolhopolov/first_program_in_java/meet_autocode/FirstProgram.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_andrii_dolhopolov.first_program_in_java.meet_autocode;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
